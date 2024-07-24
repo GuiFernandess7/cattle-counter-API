2268ef6ec6fed3eccc8b743e63848edd3b7937db
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-07-24 16:59:24 -0300

    feat: Image entity added

---
+++ src/db/entities/images.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from src.db.config.base import Base
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+class Images(Base):
+    __tablename__ = "images"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    image_path = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f"Image[id={self.id}, img_path={self.image_path}]"
