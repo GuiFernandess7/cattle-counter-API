affed7cc34a4ea9c1c66725223468dfabab78c92
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-07-24 16:25:01 -0300

    feat: Docker config added

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.9-slim
+
+RUN apt-get update && \
+    apt-get install -y curl && \
+    apt-get install -y python3-pip
+
+RUN curl -sSL https://install.python-poetry.org | python3 -
+
+ENV PATH="${PATH}:/root/.local/bin"
+
+ENV PYTHONUNBUFFERED=1
+WORKDIR /app
+
+COPY pyproject.toml poetry.lock* /app/
+
+RUN poetry config virtualenvs.create false && \
+    poetry install --no-interaction --no-ansi --no-root
+
+COPY . .
+
+EXPOSE 8000
+
+CMD ["uvicorn", "src.main:app", "--host", "0.0.0.0", "--port", "8000"]
---
+++ src/db/config/connection.py
@@ -0,0 +1,38 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+
+class DBConnectionHandler:
+
+    def __init__(self) -> None:
+        db_user = os.getenv('DB_USER', 'root')
+        db_password = os.getenv('DB_PASSWORD', 'myPassword')
+        db_host = os.getenv('DB_HOST', '172.17.0.2')
+        db_port = os.getenv('DB_PORT', '3306')
+        db_name = os.getenv('DB_NAME', 'cattleCounter')
+
+        self.__connection_string = "{}://{}:{}@{}:{}/{}".format(
+            'mysql+pymysql',
+            db_user,
+            db_password,
+            db_host,
+            db_port,
+            db_name
+        )
+        self.__engine = self.__create_database_engine()
+        self.session = None
+
+    def __create_database_engine(self):
+        engine = create_engine(self.__connection_string)
+        return engine
+
+    def get_engine(self):
+        return self.__engine
+
+    def __enter__(self):
+        session_make = sessionmaker(bind=self.__engine)
+        self.session = session_make()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
