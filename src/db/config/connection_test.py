ee32abcd380ed762f5f75e846efca5c42397c9e9
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-07-24 16:39:08 -0300

    feat: pytest condition added

--- src/db/config/connection_test.py
+++ src/db/config/connection_test.py
@@ -1,7 +1,9 @@
 import pytest
+import os
+
 from .connection import DBConnectionHandler
 
-@pytest.mark.skip(reason="Sensive test")
+@pytest.mark.skipif(os.getenv('RUN_SENSITIVE_TEST') != 'true', reason="Sensitive test")
 def test_create_database_engine():
     db_connection_handle = DBConnectionHandler()
     engine = db_connection_handle.get_engine()
