41afbdd4639cd18c89f979f76d87121786870821
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-07-25 15:26:38 -0300

    config: health check endpoint added

--- src/main.py
+++ src/main.py
@@ -3,6 +3,10 @@ from src.schemas import ImageUploadResponse
 
 app = FastAPI()
 
+@app.get("/health-check")
+async def add_nums(a: int, b: int):
+    return {"result": a + b}
+
 @app.post("/upload/image/", response_model=ImageUploadResponse)
 async def upload_image(image: UploadFile = File(...)):
     return {"message": "Image file received successfully", "filename": image.filename}
