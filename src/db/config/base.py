73f043c45182f67c72020de52c70ece962ba81a5
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-07-25 15:28:00 -0300

    Bucket environ added

--- src/db/config/base.py
+++ src/db/config/base.py
@@ -1,12 +1,11 @@
 from sqlalchemy.orm import declarative_base
-#from google.cloud import storage
+from google.cloud import storage
 import os
 
 Base = declarative_base()
 
 # GCP
-""" BUCKET_NAME = os.environ.get('BUCKET_NAME')
+BUCKET_NAME = os.environ.get('BUCKET_NAME')
 
 client = storage.Client()
 bucket = client.get_bucket(BUCKET_NAME)
- """
