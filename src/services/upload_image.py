5d127d027f78ed4a5437c5c7c49ceb6546f4b35b
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-07-26 12:30:43 -0300

    feat: New class for upload image

---
+++ src/services/upload_image.py
@@ -0,0 +1,30 @@
+#from google.cloud import storage
+#from src.db.config.base import BUCKET_NAME
+import uuid
+import os
+
+class ImageUploader:
+
+    @classmethod
+    def __get_unique_filename_path(cls, filename):
+        """Temp method"""
+        unique_filename = str(uuid.uuid4()) + os.path.splitext(filename)[1]
+        return unique_filename
+
+    @classmethod
+    def write_file(cls, filename, file_content):
+        """Temp method"""
+        unique_filename = cls.__get_unique_filename_path(filename)
+        image_path = os.path.join('media', unique_filename)
+        with open(image_path, 'wb') as f:
+            f.write(file_content)
+
+    """ @classmethod
+    def send_to_bucket(cls, file_path, unique_filename):
+        ""Not implemented""
+        client = storage.Client()
+        bucket = client.bucket(BUCKET_NAME)
+        blob = bucket.blob(unique_filename)
+
+        blob.upload_from_filename(file_path)
+        print(f"Uploaded {file_path} to {cls.GCS_BUCKET_NAME} as {unique_filename}") """
