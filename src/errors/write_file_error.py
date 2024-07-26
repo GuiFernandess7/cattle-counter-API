66ed3aa975a136ea923fdf4190f645abaa46b74e
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-07-26 13:28:10 -0300

    feat: Custom error added

---
+++ src/errors/write_file_error.py
@@ -0,0 +1,8 @@
+class WriteImageError(Exception):
+
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"WriteImageError: {self.message}"
