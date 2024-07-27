1c9abab6995952d9ce97d9b4ee70924eb692c7ab
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-07-27 13:36:51 -0300

    feat: new method for get results from image

--- src/celery_worker.py
+++ src/celery_worker.py
@@ -3,6 +3,8 @@ import time
 from celery import Celery
 from dotenv import load_dotenv
 
+from db.repositories.image_repository import ImageRepository
+
 load_dotenv(".env")
 
 app = Celery(__name__)
@@ -12,5 +14,6 @@ app.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND")
 @app.task(name="upload_image")
 def upload_image_task(filename, file_content):
     from services.upload_image import ImageUploader
-    ImageUploader.write_file(filename, file_content)
-    return "File uploaded successfully"
+    results = ImageUploader.get_results_from_image(filename=filename, file_content=file_content, min_contour_area=30)
+    #ImageRepository.insert_image(filename=filename, count=results)
+    return {"message": "Image processed", "data": results}
